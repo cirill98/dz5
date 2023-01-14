8ec14c2ab81db5b82e6ef6fe75f04bed0ffc14f0
cirill98 <100434317+cirill98@users.noreply.github.com>
2023-01-14 23:11:55 +0500

    dz

---
+++ src/personal/model/RepositoryFileJson.java
@@ -0,0 +1,11 @@
+package personal.model;
+
+import javax.xml.*;
+
+public class RepositoryFileJson {
+    public static void main(String[] args) {
+        String fileName = "C:\\Users\\79126\\Desktop\\java_dz\\dzOop5\\src";
+
+
+    }
+}
